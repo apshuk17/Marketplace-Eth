d5f3128521afdcb71826d635fb32383c41983a63
Apoorva Shukla <aposhukl@WKMZTB5622B1.global.publicisgroupe.net>
2022-03-21 09:34:59 +0530

    Add course modal to marketplace

--- components/ui/common/button/index.js
+++ components/ui/common/button/index.js
@@ -1,7 +1,8 @@
-const Button = ({ children, className, variant, hoverable=true, ...otherProps }) => {
+const Button = ({ children, className, variant='purple', hoverable=true, ...otherProps }) => {
   const colorVariants = {
     purple: hoverable ? "text-white bg-indigo-600 hover:bg-indigo-700" : "text-white bg-indigo-600",
-    red: hoverable ? "text-white bg-red-600 hover:bg-red-700" : "text-white bg-red-600"
+    red: hoverable ? "text-white bg-red-600 hover:bg-red-700" : "text-white bg-red-600",
+    lightPurple: hoverable ? "text-indigo-700 bg-indigo-100 hover:bg-indigo-200" : "text-indigo-700 bg-indigo-100"
   };
 
   return (
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { BaseLayout } from "@components/ui/layout";
 import { CourseList } from "@components/ui/course";
 import { getAllCourses } from "@content/courses/fetcher";
 import { WalletBar } from "@components/ui/web3";
 import { useWeb3 } from "@components/providers";
 import { CourseCard } from "@components/ui/course";
+import { Button } from "@components/ui/common";
+import { OrderModal } from "@components/ui/order";
 
 const Marketplace = ({ courses }) => {
   const { accountConnected, networkConnected } = useWeb3();
   const { data: accountNumber } = accountConnected;
+  const [selectedCourse, setSelectedCourse] = useState(null);
 
   const {
     data: networkName,
@@ -28,8 +32,29 @@ const Marketplace = ({ courses }) => {
         />
       </div>
       <CourseList courses={courses}>
-        {(course) => <CourseCard key={course.id} course={course} />}
+        {(course) => (
+          <CourseCard
+            key={course.id}
+            course={course}
+            Footer={() => (
+              <div className="mt-4">
+                <Button
+                  onClick={() => setSelectedCourse(course)}
+                  variant="lightPurple"
+                >
+                  Purchase
+                </Button>
+              </div>
+            )}
+          />
+        )}
       </CourseList>
+      {!!selectedCourse ? (
+        <OrderModal
+          course={selectedCourse}
+          onClose={() => setSelectedCourse(null)}
+        />
+      ) : null}
     </>
   );
 };
